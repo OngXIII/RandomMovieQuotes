5c490130b6aa14bd71ac7640ec6d7ed7ccd7db5f
OngXIII <bhuwit2538@hotmail.com>
2024-08-30 11:58:15 +0700

    Add more emoji

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const FirstMessage = ['We live in a twilight world. ', 'I am your father. ', 'My mama always said life was like a box of chocolates. ']
 const SecondMessage = ['And there are no friends at dusk. ', 'NOOOOOOOOOOOOOOOOOOOOOOO!!!! ', 'You never know what you are gonna get. ']
-const emoji = ['(^_-)', '(凸ಠ益ಠ)凸', '∩(·ω·)∩']
+const emoji = ['(^_-)', '(凸ಠ益ಠ)凸', '∩(·ω·)∩', '( ﾟ Дﾟ)']
 
 function GenRandomMessage() {
     const randomFirst = FirstMessage[Math.floor(Math.random() * FirstMessage.length)]
