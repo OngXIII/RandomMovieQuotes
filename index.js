41b3282b5beff9fbbaf786377ba5d274b096fd1d
OngXIII <bhuwit2538@hotmail.com>
2024-08-30 12:27:57 +0700

    Nest the arrays of messages

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-const FirstMessage = ['We live in a twilight world. ', 'I am your father. ', 'My mama always said life was like a box of chocolates. ']
+const messages = [
+    {
+        FirstMessage: 'We live in a twilight world. ',
+        SecondMessage: 'And there are no friends at dusk. ',
+        emoji: '(^_-)'
+    },
+    {
+        FirstMessage: 'I am your father. ',
+        SecondMessage: 'NOOOOOOOOOOOOOOOOOOOOOOO!!!! ',
+        emoji: '(凸ಠ益ಠ)凸'
+    },
+    {
+        FirstMessage: 'My mama always said life was like a box of chocolates. ',
+        SecondMessage: 'You never know what you are gonna get. ',
+        emoji: '∩(·ω·)∩'
+    },
+]
+
+function GenRandomMessage() {
+    const randomFirst = messages[Math.floor(Math.random() * messages.length)].FirstMessage;
+    const randomSecond = messages[Math.floor(Math.random() * messages.length)].SecondMessage;
+    const randomEmoji = messages[Math.floor(Math.random() * messages.length)].emoji;
+
+    return randomFirst + randomSecond + randomEmoji;
+}
+
+
+/*const FirstMessage = ['We live in a twilight world. ', 'I am your father. ', 'My mama always said life was like a box of chocolates. ']
 const SecondMessage = ['And there are no friends at dusk. ', 'NOOOOOOOOOOOOOOOOOOOOOOO!!!! ', 'You never know what you are gonna get. ']
 const emoji = ['(^_-)', '(凸ಠ益ಠ)凸', '∩(·ω·)∩', '( ﾟ Дﾟ)']
 
@@ -8,7 +35,7 @@ function GenRandomMessage() {
     const randomEmoji = emoji[Math.floor(Math.random() * emoji.length)]
 
     return randomFirst + randomSecond + randomEmoji
-}
+}*/
 
 console.log(GenRandomMessage())
 
