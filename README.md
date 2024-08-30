9b7f3d95886fe7ef7360eea61274d56a88e56c43
OngXIII <bhuwit2538@hotmail.com>
2024-08-30 11:56:08 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Random Movie Quotes with Emoji
+**About this program**
+> This program generates combination of quotes from different movies
---
+++ index.js
@@ -0,0 +1,14 @@
+const FirstMessage = ['We live in a twilight world. ', 'I am your father. ', 'My mama always said life was like a box of chocolates. ']
+const SecondMessage = ['And there are no friends at dusk. ', 'NOOOOOOOOOOOOOOOOOOOOOOO!!!! ', 'You never know what you are gonna get. ']
+const emoji = ['(^_-)', '(凸ಠ益ಠ)凸', '∩(·ω·)∩']
+
+function GenRandomMessage() {
+    const randomFirst = FirstMessage[Math.floor(Math.random() * FirstMessage.length)]
+    const randomSecond = SecondMessage[Math.floor(Math.random() * SecondMessage.length)]
+    const randomEmoji = emoji[Math.floor(Math.random() * emoji.length)]
+
+    return randomFirst + randomSecond + randomEmoji
+}
+
+console.log(GenRandomMessage())
+
